6d22524f68b9e2295b56b73ab8c30178eeb84019
anthony <diomede1991@libero.it>
2022-05-09 15:26:14 +0200

    artisan modell all comics

---
+++ laravel/database/factories/ComicsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\comics;
+use Faker\Generator as Faker;
+
+$factory->define(comics::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
