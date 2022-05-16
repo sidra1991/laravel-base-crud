a63a4862209fae283a17a73d063d05d736774fd7
anthony <diomede1991@libero.it>
2022-05-16 18:27:00 +0200

    npm install e model

---
+++ comics/app/Http/Controllers/ComicsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\comics;
+use Illuminate\Http\Request;
+
+class ComicsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\comics  $comics
+     * @return \Illuminate\Http\Response
+     */
+    public function show(comics $comics)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\comics  $comics
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(comics $comics)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\comics  $comics
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, comics $comics)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\comics  $comics
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(comics $comics)
+    {
+        //
+    }
+}
---
+++ comics/app/comics.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class comics extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'thumb',
+        'price',
+        'series',
+        'sale_date',
+        'type'
+    ];
+}
+
